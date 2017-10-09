b0ea42f3f18b30bef72ca6d9417209aae816fd36
Laura Trager <laura.trager@gmail.com>
2017-10-09 19:42:40 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -125,7 +125,7 @@ def play(board)
   end
 
 if won?(board) != nil
-    puts "Congratulations #{winner(board)}!" 
+    puts "Congratulations #{winner(board)}!"
 elsif draw?(board) == true
     puts "Cat's Game!"
 end
